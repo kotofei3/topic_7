93a0a74068f0a45047c7521712415f3c2ce3db71
Konstantin Kotov <konstantin.kotov1993@mail.ru>
2023-11-07 13:05:28 +0300

    DZ - topic - 7
    - решенные задачи 11

--- tasks/task_11.py
+++ tasks/task_11.py
@@ -1,1 +1,19 @@
-# TODO: Пожалуйста, добавьте свой код ниже с комментариями и понятными названиями переменных.
+number: int = int(input('Введите целое положительное число: '))
+
+for i in range(1, number + 1):
+    for j in range(i):
+        print(i, end=' ')
+    print()
+
+# __________________
+
+i = 1
+while i <= number:
+
+    j = 0
+    while j < i:
+        print(i, end=' ')
+        j += 1
+    print()
+
+    i += 1
