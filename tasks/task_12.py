e6e591f764770c00208d47057012b67e1bef3c25
Konstantin Kotov <konstantin.kotov1993@mail.ru>
2023-11-08 11:26:20 +0300

    DZ - topic - 7
    - задание 12 и 13 реализованны через цикл for

--- tasks/task_12.py
+++ tasks/task_12.py
@@ -1,18 +1,38 @@
-number: int = int(input('Введите целое положительное число: '))
+# number: int = int(input('Введите целое положительное число: '))
 
 
-
-for i in range(1, number + 1):
-    spase = number - i
-
-    for j in range(spase):
-        print(' ', end=' ')
-
-    for _ in range(2 * i - 1):
-        print('*', end=' ')
-
-    print()
+# for i in range(1, number + 1):
+#     spase = number - i
+#
+#     for j in range(spase):
+#         print(' ', end=' ')
+#
+#     for _ in range(i * 2 - 1):
+#         print(i, end=' ')
+#
+#     print()
 
 # -------------
 
 # todo: реализовать через цикл while
+num: int = int(input('Введите целое положительное число: '))
+i: int = 1
+count: int = 0
+while i <= num:
+    count += 1
+
+    space = num - i
+
+    j = 1
+    while j <= space:
+        print(' ', end='')
+        j += 1
+
+    k = 0
+    while k < i:
+        print('*', end=' ')
+        k += 1
+    print()
+
+    i += 1
+print('iter osnov ciclsa', count)
